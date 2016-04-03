8c2a038af009dcef149e3d1166580322115ab2b7
Steven Kim <stevensoojinkim@gmail.com>
2016-04-03 19:41:09 -0400

    open new tab

--- ui.R
+++ ui.R
@@ -9,7 +9,7 @@ shinyUI(fluidPage(
       textInput("query", label = h3("N-Gram input"), value = ""),
       actionButton("search", label = "Search"),
       helpText("Use ',' to separate queries, and '+' to combine queries. E.g., querying 'a, b' will search for both 'a' and 'b'; querying 'a + b' will combine occurrences of 'a' and 'b'. All excess whitespace is stripped, and queries are case-insensitive. "),
-      helpText(   a("About",     href="https://github.com/mikss/nsfads/blob/master/README.md")
+      helpText(   a("About",     href="https://github.com/mikss/nsfads/blob/master/README.md", target="_blank")
       )
     ),
     
